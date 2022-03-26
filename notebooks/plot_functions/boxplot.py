f9a2b08f0f214288de7385ad5cd9275672141895
Elias <elias.galiounas.19@ucl.ac.uk>
2022-03-26 14:11:05 +0000

    In perparation for final submission

---
+++ notebooks/plot_functions/boxplot.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import plotly.express as px
+
+def plot(selected_var, resampling, df):
+    mean_per_freq = df[[selected_var,'battery_id']].groupby('battery_id').resample(resampling)[selected_var].agg('mean')
+    mean_per_freq = mean_per_freq.reset_index()
+    mean_per_freq['timestamp'] = mean_per_freq['timestamp'].dt.date
+    mean_per_freq = mean_per_freq.sort_values('timestamp')
+    fig = px.box(mean_per_freq, x = 'timestamp', y = selected_var, title = f'Mean {selected_var} per battery per month')
+    fig.update_layout(title_x = 0.5)
+    #, plot_bgcolor = '#d2d9d7')
+    return fig
---
+++ notebooks/plot_functions/median_profiles.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+def prepare_data(df, resample = True):
+    df=df.copy()
+    if resample == True:
+        df = df.groupby('battery_id').resample('10min').mean()
+        df = df.drop('battery_id', axis = 1)
+
+        df = df.reset_index().set_index('timestamp')
+
+    df['hour'] = df.index.hour
+    df['minutes'] = df.index.minute
+    df['day_n'] = df.index.weekday
+
+    return df
+
+def plot(field, n_batteries, df):
+    freq_group_by = ['hour','minutes'] #also ['day_n','hour','minutes']
+    agg_method = 'mean'
+
+    DF = prepare_data(df[[field,'battery_id']])
+    randoms_ids = np.random.choice(DF.battery_id.unique(), n_batteries)
+    subset = DF[DF['battery_id'].isin(randoms_ids)]
+
+    df_median = subset.groupby(['battery_id'] + freq_group_by).agg(agg_method)
+    df_plot = df_median.reset_index().set_index(freq_group_by)#[[field]].unstack(level = 0).sort_index()
+    df_plot.index = df_plot.index.map('{0[0]}H{0[1]}'.format) 
+    fig = px.line(df_plot, y = field, color = 'battery_id', title = f'Median {field} profile')
+    fig.update_layout(title_x = 0.5, showlegend=False)
+    return fig
